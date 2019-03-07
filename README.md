d0ef49a3f61274f62b360555398f699bb5f5bc16
Daniel Gillespie <daniel.gillespie@pluto.tv>
2019-03-07 14:17:29 -0800

    initial import of project

---
+++ README.md
@@ -0,0 +1,52 @@
+# hapi-modern-cors
+
+Dead simple CORS responses for Hapi.
+
+### Description
+
+As of this writing (March 2019) there are no working hapi CORS npm packages available.
+There was a major refactor of the hapi project in v17 and beyond that broke existing paradigms.
+
+### Installation
+
+    npm install hapi-modern-cors --save
+
+### Usage
+
+Add this to your hapi initialization:
+
+    // ...
+    const cors = require('hapi-modern-cors');
+    // ...
+
+    await server.register(...);
+
+    // routes and config should go here BEFORE calling the plugin
+    // and finally ...
+
+    await server.register({
+        plugin: cors,
+        options: {},
+    });
+
+    await server.start();
+
+### Notes
+
+##### How do CORS pre-flight work?
+
+Before a CORS request start a pre-flight request is typically sent. This is a request using the `OPTIONS` keyword
+and the response to this request should contain some default CORS headers. This plugin will automatically create all
+`OPTIONS` routes to match your existing endpoints.
+
+##### What happens when the origin is set?
+
+When `origin` is set in the request header then the response will set the origin from `*` to that origin and
+`Allow-Credentials` will be set from `false` to `true`. For security reasons, `Allow-Credentials` cannot be set to
+`true` when `*` is used for `origin`.
+
+### License
+
+Licensed under the GNU GENERAL PUBLIC LICENSE (Version 3, 29th June 2007)
+
+https://www.gnu.org/licenses/gpl-3.0.en.html
---
+++ index.js
@@ -0,0 +1,71 @@
+const defaultCorsHeaders = [{
+    key: 'Access-Control-Allow-Methods',
+    value: 'GET,POST,PUT,DELETE',
+}, {
+    key: 'Access-Control-Allow-Headers',
+    value: 'Accept, Authorization, Content-Type, If-None-Match, X-Requested-With',
+}, {
+    key: 'Access-Control-Max-Age',
+    value: 1728000,
+}];
+
+function init(server) {
+    // setup the CORS pre-flight request paths on all available endpoints
+
+    const routes = server.table().map(item => item.path);
+    // if there is a GET and POST, etc then remove dupes at same path.
+    const dedupeRoutes = [...(new Set(routes))];
+
+    for (const route of dedupeRoutes) {
+        server.route({
+            method: 'OPTIONS',
+            path: route,
+            config: {
+                cors: {
+                    maxAge: 1728000,
+                    headers: ['Origin', 'Accept', 'X-Requested-With', 'Content-Type'],
+                    credentials: false,
+                    origin: ['*'],
+                },
+                handler() {
+                    return {
+                        cors: 'true',
+                    };
+                },
+            },
+        });
+    }
+}
+
+const cors = {
+    name: 'cors',
+    version: '1.0.0',
+    async register(server) {
+        init(server);
+
+        server.ext('onPreResponse', async (request, h) => {
+            let origin = '*';
+            let allowCreds = false;
+
+            if (request.headers.origin) {
+                // CORS spec requires 'Allow-Credentials' header can't be set
+                // to 'true' when using '*" in 'Allow-Origin' for security purposes.
+                origin = request.headers.origin; // eslint-disable-line prefer-destructuring
+                allowCreds = true;
+            }
+
+            // boom requests handles response obj differently
+            const response = request.response.isBoom ? request.response.output : request.response;
+
+            response.headers['Access-Control-Allow-Origin'] = origin;
+            response.headers['Access-Control-Allow-Credentials'] = allowCreds;
+            for (const defaultHeader of defaultCorsHeaders) {
+                response.headers[defaultHeader.key] = defaultHeader.value;
+            }
+
+            return h.continue;
+        });
+    },
+};
+
+module.exports = cors;
